0ffce8254c89313f22aacdb84837170f61bb0197
InderKhera <inder.khera@hotmail.com>
2025-01-09 22:36:06 -0800

    Update pooling_image.py

--- src/sharpedge/pooling_image.py
+++ src/sharpedge/pooling_image.py
@@ -2,9 +2,9 @@ def pooling_image(image, window_size, func=np.mean, color_map='gray'):
     """
     Perform pooling on an image using a specified window size and pooling function.
     
-    This function reduces the size of an input image, by dividing it into non-overlapping 
-    windows of a specified size by implementing a pooling function (e.g., mean, max, or min) 
-    to each window. The pooled image is then returned as displayed with a specific colormap.
+    This function reduces the size of an input image, by dividing the image into windows of a 
+    specified size by implementing a pooling function (e.g., mean, max, or min) to each window. 
+    The pooled image is then returned and displayed with a specific colormap.
     
     Parameters:
     image (ndarray): The input image as a 2D numpy array (grayscale) or a 3D numpy 
@@ -16,9 +16,9 @@ def pooling_image(image, window_size, func=np.mean, color_map='gray'):
                                 Common functions used are np.mean, np.median, np.max, np.min.
                                 Default is np.mean.
         
-    color_map (str, optional): The colormap to use for displaying the pooled image using matplotlib. Examples include:
-                               'gray', 'inferno', 'virdis', 'plasma', etc.
-                                Default is `'gray'`.
+    color_map (str, optional): The colormap to use for displaying the pooled image using matplotlib. 
+                               Examples include:'gray', 'inferno', 'virdis', 'plasma', etc.
+                               Default is `'gray'`.
     
     Returns:
     matplotlib.image: The pooled image displayed with the specified colormap.
