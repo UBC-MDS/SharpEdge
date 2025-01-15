f6ddc3d0c6a43e4534fd04f751b8c458dba0b072
Hankun Xiao <xh1319@student.ubc.ca>
2025-01-14 22:57:36 -0800

    update utility file structure and add utility test

--- src/sharpedge/sample_input_checker.py
+++
@@ -1,5 +0,0 @@
-from Utility import Utility
-
-# how to call the image_sanity_check function
-result = Utility._input_checker(1)
-print(result)
---
+++ tests/test_utility.py
@@ -0,0 +1,4 @@
+from sharpedge._utils import Utility
+
+result = Utility._input_checker([1]) # call the _input_checker function
+print(result)
