08226c1c71cce593d85f515a9e429f18866aae90
Rongze Liu <arccelt@zju.edu.cn>
2025-01-17 01:17:29 -0800

    test: write test data and code for edge cases, modify some test images, fix the path problem in pytest

--- tests/test_seam_carving.py
+++ tests/test_seam_carving.py
@@ -1,15 +1,20 @@
+import os
 import numpy as np
 import pytest
 import matplotlib.pyplot as plt
 from sharpedge.seam_carving import seam_carve
 
+test_image_dir = os.path.join(os.path.dirname(__file__), "test_image")
 
 # Test data
-input_1 = plt.imread("test_image/seam_carve_input_1.png")[:, :, :3]
-input_3 = plt.imread("test_image/seam_carve_input_3.png")[:, :, :3]
-output_1 = plt.imread("test_image/seam_carve_output_1.png")[:, :, :3]
-output_2 = plt.imread("test_image/seam_carve_output_2.png")[:, :, :3]
-output_3 = plt.imread("test_image/seam_carve_output_3.png")[:, :, :3]
+input_1 = plt.imread(os.path.join(test_image_dir, "seam_carve_input_1.png"))[:, :, :3]
+input_2 = plt.imread(os.path.join(test_image_dir, "seam_carve_input_2.png"))[:, :, :3]
+input_3 = plt.imread(os.path.join(test_image_dir, "seam_carve_input_3.png"))[:, :, :3]
+output_1_1 = plt.imread(os.path.join(test_image_dir, "seam_carve_output_1_1.png"))[:, :, :3]
+output_1_2 = plt.imread(os.path.join(test_image_dir, "seam_carve_output_1_2.png"))[:, :, :3]
+output_1_3 = plt.imread(os.path.join(test_image_dir, "seam_carve_output_1_3.png"))[:, :, :3]
+output_2 = plt.imread(os.path.join(test_image_dir, "seam_carve_output_2.png"))[:, :, :3]
+output_3 = plt.imread(os.path.join(test_image_dir, "seam_carve_output_3.png"))[:, :, :3]
 
 
 # Expected cases
@@ -24,11 +29,64 @@ def test_seam_carve_expected_shape(img, target_height, target_width, expected_sh
 
 
 @pytest.mark.parametrize("img, target_height, target_width, expected_array", [
-    (input_1, 7, 7, output_1),
-    (input_1, 5, 4, output_2),
-    (input_3, 1000, 1000, output_3)
+    (input_1, 7, 7, output_1_1),
+    (input_1, 5, 4, output_1_2),
+    (input_2, 1000, 1000, output_2)
 ])
 def test_seam_carve_expected_array(img, target_height, target_width, expected_array):
     result = seam_carve(img, target_height, target_width)
     if expected_array.size > 0:
         np.testing.assert_array_almost_equal(result, expected_array)
+
+
+# Edge cases
+# Edge case 1: no resizing in height or width needed, expect warnings
+@pytest.mark.parametrize("img, target_height, target_width, warning_msg", [
+    (np.random.rand(10, 10, 3), 10, 10, "Both target height and width are the same as that of the original image. No resizing needed."),
+    (np.random.rand(10, 10, 3), 10, 5, "Target height is the same as the original height."),
+    (np.random.rand(10, 10, 3), 5, 10, "Target width is the same as the original width."),
+])
+def test_seam_carve_no_resize(img, target_height, target_width, warning_msg):
+    with pytest.warns(UserWarning, match=warning_msg):
+        result = seam_carve(img, target_height, target_width)
+
+
+# Edge case 2: seam carving with a single pixel image, expect warnings
+@pytest.mark.parametrize("img, target_height, target_width, warning_msg", [
+    (np.random.rand(1, 1, 3), 1, 1, "Image is already a single pixel."),
+])
+def test_seam_carve_single_pixel_input(img, target_height, target_width, warning_msg):
+    with pytest.warns(UserWarning, match=warning_msg):
+        result = seam_carve(img, target_height, target_width)
+
+
+# Edge case 3: target width and height are 1, expect warnings
+@pytest.mark.parametrize("img, target_height, target_width, warning_msg", [
+    (np.random.rand(5, 8, 3), 1, 1, "Warning! Resizing to a single pixel."),
+    (np.random.rand(10, 4, 3), 1, 1, "Warning! Resizing to a single pixel."),
+    (np.random.rand(20, 15, 3), 1, 1, "Warning! Resizing to a single pixel."),
+    (np.random.rand(2, 2, 3), 1, 1, "Warning! Resizing to a single pixel.")
+])
+def test_seam_carve_single_pixel_target_warning(img, target_height, target_width, warning_msg):
+    with pytest.warns(UserWarning, match=warning_msg):
+        result = seam_carve(img, target_height, target_width)
+
+
+@pytest.mark.parametrize("img, target_height, target_width, expected_array", [
+    (input_1, 1, 1, output_1_3),
+    (input_3, 1, 1, output_3),
+])
+def test_seam_carve_single_pixel_target_output(img, target_height, target_width, expected_array):
+    result = seam_carve(img, target_height, target_width)
+    np.testing.assert_array_almost_equal(result, expected_array)
+
+
+# Edge case 4: seam carving with significant resizing, expect warnings
+@pytest.mark.parametrize("img, target_height, target_width, warning_msg", [
+    (np.random.rand(202, 40, 3), 2, 30, "Significant resizing is required. It may take a long while."),
+    (np.random.rand(10, 300, 3), 5, 100, "Significant resizing is required. It may take a long while."),
+    (np.random.rand(203, 204, 3), 3, 4, "Significant resizing is required. It may take a long while."),
+])
+def test_seam_carve_significant_resizing(img, target_height, target_width, warning_msg):
+    with pytest.warns(UserWarning, match=warning_msg):
+        result = seam_carve(img, target_height, target_width)
